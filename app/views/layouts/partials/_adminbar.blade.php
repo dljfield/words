f8fff74bd2dd1780d34a3d08a035199ab853edd8
David Field <david@thedebonair.net>
2014-09-07 18:12:59 +0100

    Styled the admin bar.

--- app/views/layouts/partials/_adminbar.blade.php
+++ app/views/layouts/partials/_adminbar.blade.php
@@ -1,7 +1,9 @@
-<nav>
-    <ul>
-        <li>{{ link_to_route('posts.create', 'Make New Post') }}</li>
-        <li>{{ link_to_route('admin.index', 'Admin Section') }}</li>
-        <li>{{ link_to_route('logout', 'Log Out') }}</li>
-    </ul>
+<nav class="row admin-bar">
+    <div class="small-12 columns">
+        <ul>
+            <li>{{ link_to_route('posts.create', 'Make New Post') }}</li>
+            <li>{{ link_to_route('admin.index', 'Admin Section') }}</li>
+            <li>{{ link_to_route('logout', 'Log Out') }}</li>
+        </ul>
+    </div>
 </nav>
