c3ed7627d905312e984ee7bbadbddcbe47557c75
David Field <david@thedebonair.net>
2014-09-10 21:06:31 +0100

    Adjusting links.

--- app/views/layouts/partials/_navigation.blade.php
+++ app/views/layouts/partials/_navigation.blade.php
@@ -1,7 +1,8 @@
 <nav class="row nav-main">
     <div class="small-12 columns">
         <ul>
-            <li>{{ link_to_route('home', 'Home') }}</li>
+            <li><a href="http://thedebonair.net">Home</a></li>
+            <li>{{ link_to_route('home', 'Latest Posts') }}</li>
             <li>{{ link_to_route('posts.index', 'All Posts') }}</li>
         </ul>
     </div>
