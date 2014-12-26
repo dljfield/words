76be8faa077524679e4a193dc367556d844e5d17
David Field <david@thedebonair.net>
2014-12-26 17:46:49 +0000

    Removing old CSS things

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -2,25 +2,20 @@
 
 @section('content')
 
+    <article>
+        <h2> {{$post->title}} </h2>
+        <div>{{ $post->summary }}</div>
+        <div>{{ $post->body }}</div>
+    </article>
 
-        <article>
-            <h2> {{$post->title}} </h2>
-            <div>{{ $post->summary }}</div>
-            <div>{{ $post->body }}</div>
-        </article>
-
-
-
-        <section>
-            <h4>Post Info</h4>
-            <p>Posted: {{ $post->created_at }} </p>
-            @if (Auth::check())
-                <div>{{ link_to_route('posts.edit', 'Edit This Post', $post->id) }}</div>
-            @endif
-            <div>{{ link_to_route('posts.index', '&laquo; All Posts') }}</div>
-        </section>
-
-    </div>
+    <section>
+        <h4>Post Info</h4>
+        <p>Posted: {{ $post->created_at }} </p>
+        @if (Auth::check())
+            <div>{{ link_to_route('posts.edit', 'Edit This Post', $post->id) }}</div>
+        @endif
+        <div>{{ link_to_route('posts.index', '&laquo; All Posts') }}</div>
+    </section>
 
     <div>
         <div>{{ link_to_route('posts.index', '&laquo; All Posts') }}</div>
