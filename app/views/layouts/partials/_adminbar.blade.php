8d4d71fd75640b88b8d30d79433b67791cdef1ae
David Field <david@thedebonair.net>
2014-12-26 20:21:28 +0000

    Admin bar updated.

--- app/views/layouts/partials/_adminbar.blade.php
+++ app/views/layouts/partials/_adminbar.blade.php
@@ -1,7 +1,13 @@
-<nav>
-    <ul>
-        <li>{{ link_to_route('posts.create', 'Make New Post') }}</li>
-        <li>{{ link_to_route('admin.index', 'Admin Section') }}</li>
-        <li>{{ link_to_route('logout', 'Log Out') }}</li>
-    </ul>
-</nav>
+<div class="row">
+    <div class="twelve columns">
+		<div class="admin-bar">
+			<nav>
+			    <ul>
+			        <li>{{ link_to_route('posts.create', 'Make New Post') }}</li>
+			        <li>{{ link_to_route('admin.index', 'Admin Section') }}</li>
+			        <li>{{ link_to_route('logout', 'Log Out') }}</li>
+			    </ul>
+			</nav>
+		</div>
+	</div>
+</div>
